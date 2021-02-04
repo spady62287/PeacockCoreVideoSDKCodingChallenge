fa89046f227ebb10e77619051ba128045e65a816
Daniel Spady <danielspady@gmail.com>
2021-02-04 13:18:39 -0500

    Initial Commit

---
+++ PeacockCoreVideoSDKCodingChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PeacockCoreVideoSDKCodingChallenge
+//
+//  Created by Daniel Spady on 2021-02-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PeacockCoreVideoSDKCodingChallenge/PeacockCoreVideoSDKCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeacockCoreVideoSDKCodingChallengeApp.swift
+//  PeacockCoreVideoSDKCodingChallenge
+//
+//  Created by Daniel Spady on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct PeacockCoreVideoSDKCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
